d2da7a76b782099e4ca62c894b5258afff901120
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-17 14:16:22 +0100

    render based on current view

--- frontend/src/Components/App/Index.jsx
+++ frontend/src/Components/App/Index.jsx
@@ -1,15 +1,27 @@
 import SideBar from '../Sidebar/Index'
 import Content from '../Movies/Index'
 
+import { useState as UseState } from 'react'
+
 import './Index.css'
 
 function App() {
-  return (
-    <div className="App container-fluid px-0">
-        <SideBar />
-        <Content />
-    </div>
-  )
+    const [CurrentView, SetCurrentView] = UseState("Movies")
+
+    let ContentElement
+
+    if (CurrentView == "Movies") {
+        ContentElement = <Content />
+    }
+
+    console.log(ContentElement)
+
+    return (
+        <div className="App container-fluid px-0">
+            <SideBar CurrentView={CurrentView} SetCurrentView={SetCurrentView} />
+            {ContentElement}
+        </div>
+    )
 }
 
 export default App
