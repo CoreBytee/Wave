780ffd8fda90620973c1ef46a9601b2d0b810253
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-17 14:18:28 +0100

    rm bootstrap classes

--- frontend/src/Components/App/Index.jsx
+++ frontend/src/Components/App/Index.jsx
@@ -15,7 +15,7 @@ function App() {
     }
 
     return (
-        <div className="App container-fluid px-0">
+        <div className="App">
             <SideBar CurrentView={CurrentView} SetCurrentView={SetCurrentView} />
             {ContentElement}
         </div>
