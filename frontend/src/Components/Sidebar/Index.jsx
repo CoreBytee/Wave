03b7968335d471fabcc66188ddb6f8e1b562ddb1
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-17 15:01:59 +0100

    set view

--- frontend/src/Components/Sidebar/Index.jsx
+++ frontend/src/Components/Sidebar/Index.jsx
@@ -1,8 +1,20 @@
 import './Index.css'
 
-function SideBar() {
+import Icon from '../../Assets/icon.svg'
+
+function SideBar(
+    {
+        CurrentView,
+        SetCurrentView
+    }
+) {
     return (
-        <div className="SideBar"></div>
+        <div className="SideBar">
+            <img src={Icon} className="logo" alt="Logo" />
+            <i onClick={() => SetCurrentView("Movies")} className="bi bi-house "></i>
+            <i onClick={() => SetCurrentView("Saved")}  className="bi bi-heart "></i>
+            <i onClick={() => SetCurrentView("Search")} className="bi bi-search"></i>
+        </div>
     )
 }
 
