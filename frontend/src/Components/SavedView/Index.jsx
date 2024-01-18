f13a8681e7860104d94287b647cc30b95b1f74ba
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-18 14:03:18 +0100

    add movies to savedview

--- frontend/src/Components/SavedView/Index.jsx
+++ frontend/src/Components/SavedView/Index.jsx
@@ -1,14 +1,72 @@
+import { useState as UseState } from 'react'
+
+import MovieCard from '../MovieCard/Index'
+
 import './Index.css'
 
 import FrownImage from '../../assets/frown.svg'
 
 function SavedView() {
+    const [Movies, SetMovies] = UseState([])
+
+    async function FetchMovies() {
+        const MovieResponse = await fetch("https://annexbios.gluwebsite.nl/api/main.php")
+        const MovieData = await MovieResponse.json()
+
+        SetMovies(MovieData)
+    }
+
+    if (Movies.length === 0) {
+        FetchMovies()
+        return null
+    }
+
+    const SavedMovies = []
+
+    Movies.forEach(
+        Movie => {
+            if (localStorage[`MovieFavourite-${Movie.id}`] === "true") {
+                SavedMovies.push(Movie)
+            }
+        }
+    )
+
+    if (SavedMovies.length == 0) {
+        return (
+            <div className="SavedView">
+                <div className='nosaved'>
+                    <img src={FrownImage} />
+                    <h6>Looks like you do not have any saved movies at this time!</h6>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="SavedView">
-            <img src={FrownImage} />
-            <h6>Looks like you do not have any saved movies at this time!</h6>
+            <h1>Movies that you have saved</h1>
+            <div className='cardlist'>
+                {
+                    (
+                        SavedMovies.map(
+                            Movie => {
+                                return (
+                                    <MovieCard
+                                        key={Movie.id}
+                                        Id={Movie.id}
+                                        Title={Movie.title}
+                                        ImgUrl={Movie.poster}
+                                    />
+                                )
+                            }
+                        )
+                    )
+                }
+            </div>
         </div>
     )
+
+
 }
 
 export default SavedView;
