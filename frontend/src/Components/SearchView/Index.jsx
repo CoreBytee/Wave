95e4f30722a47365dfaeb9a7e4ce3905be7167c4
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-18 09:14:12 +0100

    complete searchview

--- frontend/src/Components/SearchView/Index.jsx
+++ frontend/src/Components/SearchView/Index.jsx
@@ -1,9 +1,51 @@
+import { useEffect as UseEffect, useState as UseState } from 'react'
+
+import MovieCard from '../MovieCard/Index'
+
 import './Index.css'
 
 function SearchView() {
-  return (
-    <div className="SearchView"></div>
-  )
+    const [Movies, SetMovies] = UseState([])
+
+    UseEffect(
+        () => {
+            async function FetchMovies() {
+                const MovieResponse = await fetch("https://annexbios.gluwebsite.nl/api/main.php")
+                const MovieData = await MovieResponse.json()
+
+                console.log(MovieData)
+                SetMovies(MovieData)
+            }
+
+            if (Movies.length === 0)
+                FetchMovies()
+        }
+    )
+
+
+
+    return (
+        <div className="SearchView">
+            <input type="text" placeholder="Enter query" ></input>
+            <div className='cardlist'>
+                {
+                    (
+                        Movies.map(
+                            Movie => {
+                                return (
+                                    <MovieCard
+                                        Id={Movie.id}
+                                        Title={Movie.title}
+                                        ImgUrl={Movie.poster}
+                                    />
+                                )
+                            }
+                        )
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default SearchView
