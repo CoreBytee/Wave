bcb24fbc42fec772631925a334e78d972391c418
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-18 13:09:16 +0100

    clean code

--- frontend/src/Components/SearchView/Index.jsx
+++ frontend/src/Components/SearchView/Index.jsx
@@ -1,4 +1,4 @@
-import { useEffect as UseEffect, useState as UseState } from 'react'
+import { useState as UseState } from 'react'
 
 import MovieCard from '../MovieCard/Index'
 
@@ -7,21 +7,17 @@ import './Index.css'
 function SearchView() {
     const [Movies, SetMovies] = UseState([])
 
-    UseEffect(
-        () => {
-            async function FetchMovies() {
-                const MovieResponse = await fetch("https://annexbios.gluwebsite.nl/api/main.php")
-                const MovieData = await MovieResponse.json()
+    async function FetchMovies() {
+        const MovieResponse = await fetch("https://annexbios.gluwebsite.nl/api/main.php")
+        const MovieData = await MovieResponse.json()
 
-                SetMovies(MovieData)
-            }
+        SetMovies(MovieData)
+    }
 
-            if (Movies.length === 0)
-                FetchMovies()
-        }
-    )
-
-
+    if (Movies.length === 0) {
+        FetchMovies()
+        return null
+    }
 
     return (
         <div className="SearchView">
