de2a1b31355fca9c3cfff86c072be0f4fa2e5beb
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-18 10:31:18 +0100

    add frown image to savedview

--- frontend/src/Components/SavedView/Index.jsx
+++ frontend/src/Components/SavedView/Index.jsx
@@ -1,9 +1,14 @@
 import './Index.css'
 
+import FrownImage from '../../assets/frown.svg'
+
 function SavedView() {
-  return (
-    <div className="SavedView"></div>
-  )
+    return (
+        <div className="SavedView">
+            <img src={FrownImage} />
+            <h6>Looks like you do not have any saved movies at this time!</h6>
+        </div>
+    )
 }
 
 export default SavedView;
